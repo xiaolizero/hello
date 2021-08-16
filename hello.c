0925293d9fac3ac328f6afd0c85e0b8614a4bfdb
forlinx <li2390707440@outlook.com>
2021-08-16 09:28:19 +0800

    first commit

--- hello.c
+++ hello.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
-
 int main(int argc,char **argv)
 {
-printf("hellow word\n");
-printf(" %d\n %s\n",argc,argv[1]);
-return 0;
-
+	printf("hello word\n%d\n%s\n",argc,argv[1]);
+	return 0;
 }
